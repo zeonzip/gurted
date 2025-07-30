2fd69068cd0965d1786310045d1a75dbcf0f6b96
Face <69168154+face-hh@users.noreply.github.com>
2025-07-30 21:06:40 +0300

    remove redundant dedicated h1-h6 & pre .gd & .tscn

--- Scripts/Tags/h3.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-@onready var rich_text_label: RichTextLabel = $RichTextLabel
-
-func init(_element: HTMLParser.HTMLElement) -> void:
-	pass
--- Scripts/Tags/pre.gd
+++
@@ -1,5 +0,0 @@
-class_name Pre
-extends VBoxContainer
-
-func init(_element: HTMLParser.HTMLElement) -> void:
-	pass
